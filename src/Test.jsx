566353673316caf9960cc8879aeefee7c7523fce
Nabakumr <nabakumarbanerjee70@gmail.com>
2026-03-26 14:49:14 +0530

    Auto Commit Before Push

---
+++ src/Test.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  const name = "Vercel Test"
+
+  return (
+    <div>
+      <h1>Hello {name}</h1>
+      <p>This will fail in Vercel</p>
+
+    </div>  // ❌ Missing closing bracket below
+}
